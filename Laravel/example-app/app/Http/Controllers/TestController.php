40e03cc66d5925d570d2bb73e5bc13cc552b2db1
Ruslan Parkhomchuk <parkhomchuk.lol@gmail.com>
2025-03-06 20:05:37 +0200

    Lab01

---
+++ Laravel/example-app/app/Http/Controllers/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TestController extends Controller
+{
+    /**
+     * @return string
+     */
+    public function test(): string {
+        return "test";
+    }
+}
